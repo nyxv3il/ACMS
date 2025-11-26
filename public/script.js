fbaf719a96a40710164ccb3c2c7309df35d9c353
Nyx <pho3nix.botany038@passinbox.com>
2025-11-26 19:02:17 +0530

    added footer

--- public/script.js
+++ public/script.js
@@ -79,3 +79,8 @@ document.getElementById("joinusBtn").addEventListener("click", (e) => {
     "_blank"
   );
 });
+
+const currentYearEl = document.getElementById("currentYear");
+if (currentYearEl) {
+  currentYearEl.textContent = new Date().getFullYear();
+}
